94e1ea1a50b84efa6c83be56a3485996b0b99995
Jatin K Malik <jmalik@atlassian.com>
2023-12-16 19:50:50 +0530

    Update code examples

--- src/13_cryptic_algorithms.py
+++ src/13_cryptic_algorithms.py
@@ -4,3 +4,5 @@ def cryptic_algo(a, b):
     enigmatic_shift = ((mystical_transform << 3) & 0xFF) | obscure_factor
     cryptic_result = (enigmatic_shift ^ 0xDEAD) - ((a & b) * (obscure_factor % 3))
     return cryptic_result
+
+print(cryptic_algo(1,2))
